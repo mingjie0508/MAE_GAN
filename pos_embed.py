1d2144fb30d710ea87ef2e694935dcab71ce9450
Mingjie Zhao <m223zhao@uwaterloo.ca>
2025-01-13 17:57:32 -0500

    Add project document

--- pos_embed.py
+++ pos_embed.py
@@ -4,7 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
-# Position embedding utils
+# Reference:
+# MAE: https://github.com/facebookresearch/mae
 # --------------------------------------------------------
 
 import numpy as np
